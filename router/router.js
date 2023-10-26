cb63be0ec174994a3f08dc43250fdab858f87b60
M A HARISH J <mr.horror3737@gmail.com>
2023-10-26 20:42:11 +0530

    router config

---
+++ router/router.js
@@ -0,0 +1,8 @@
+const router  = require('mongoose').Router()
+
+const {getOne, getAll, putOne, updateOne, delOne} = require('../controller/handler.js')
+
+router.route('/').get(getAll).put(putOne)
+router.route('/:testId').get(getOne).update(updateOne).delete(delOne)
+
+module.exports = router
