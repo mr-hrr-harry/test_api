69d474b692a3a6ddeb39faf5810c1103c8af6b38
M A HARISH J <mr.horror3737@gmail.com>
2023-10-26 20:58:57 +0530

    handler creation pseudo

---
+++ controller/handler.js
@@ -0,0 +1,38 @@
+const schema = require('../model/schema.js')
+
+//retrieve one user data
+//Method: findOne()
+//GET request
+const getOne = async(req, res) => {
+
+}
+
+//retrieve all users data
+//Method: find()
+//GET request
+const getAll = async(req, res) => {
+
+}
+
+//store one user data
+//Method: create(), validate(), save()
+//PUT request
+const putOne = async(req, res) => {
+
+}
+
+//update one user data
+//Method: findOneAndUpdate()
+//PATCH request
+const updateOne = async(req, res) => {
+
+}
+
+//delete one user data
+//Method: deleteMany()
+//DELETE request
+const delOne = async(req, res) => {
+
+}
+
+module.exports = {getOne, getAll, putOne, updateOne, delOne}
--- router/router.js
+++ router/router.js
@@ -2,6 +2,7 @@ const router  = require('mongoose').Router()
 
 const {getOne, getAll, putOne, updateOne, delOne} = require('../controller/handler.js')
 
+//router config
 router.route('/').get(getAll).put(putOne)
 router.route('/:testId').get(getOne).update(updateOne).delete(delOne)
 
